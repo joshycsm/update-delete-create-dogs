1a7033c982d176646b8144e8cf4af82e5052a7c7
github username <github email address>
2020-02-21 10:09:18 -0700

    fixes updating age of any dogs

--- app.js
+++ app.js
@@ -4,47 +4,48 @@ const dogForm = document.querySelector("#dog-form");
 
 fetch(`${BASE_URL}`)
   .then(response => response.json())
-  .then(dogs =>
-    dogs.forEach(dog => {
-      const dogCard = document.createElement("div");
-      dogCard.innerHTML = `
+  .then(dogs => dogs.forEach(dog => {
+    const dogCard = document.createElement('div')
+    dogCard.innerHTML = `
         <div class="dog-info">
             <img src="${dog.image}" />
             <h1>Dog Name: ${dog.name}</h1>
             <h1>Breed: ${dog.breed}</h1>
             <h1>Age: ${dog.age}</h1>
         </div>
-        `;
-      const deleteButton = document.createElement("button");
-      deleteButton.textContent = "DELETE";
-      dogCard.appendChild(deleteButton);
-      deleteButton.addEventListener("click", () => {
-        event.target.parentNode.remove();
-        fetch(`${BASE_URL}/${dog.id}`, {
-          method: "DELETE"
-        });
-      });
-      const editDog = document.createElement("form");
-      editDog.innerHTML = `
-        <input type="number" id="age" name="age" value=${dog.age}>
-        `;
-      editDog.addEventListener("submit", () => {
-        event.preventDefault();
-        const newAge = document.getElementById("age").value;
-        fetch(`${BASE_URL}/${dog.id}`, {
-          method: "PATCH",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({ age: newAge })
-        });
-      });
-      dogCard.append(editDog, deleteButton);
-      dogsContainer.appendChild(dogCard);
+        `
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'DELETE'
+    dogCard.appendChild(deleteButton)
+    deleteButton.addEventListener('click', () => {
+      event.target.parentNode.remove()
+      fetch(`${BASE_URL}/${dog.id}`, {
+        method: 'DELETE'
+      })
     })
-  )
-  .catch(console.error);
+    const editDog = document.createElement('form')
+    editDog.innerHTML = `
+        <input type="number" id="dog${dog.id}" name="age" value=${dog.age} />
+        `
+    editDog.addEventListener('submit', () => {
+      event.preventDefault()
+      const newAge = document.getElementById(`dog${dog.id}`).value
+      console.log(newAge, dog.id, event.target)
+      fetch(`${BASE_URL}/${dog.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          age: newAge
+        })
+      })
+    })
+    dogCard.append(editDog, deleteButton)
+    dogsContainer.appendChild(dogCard)
+  }))
+  .catch(console.error)
 
 dogForm.addEventListener("submit", () => {
   event.preventDefault();
@@ -59,7 +60,7 @@ dogForm.addEventListener("submit", () => {
   fetch(BASE_URL, {
     method: "POST",
     headers: {
-      Accept: "application/json",
+      "Accept": "application/json",
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
